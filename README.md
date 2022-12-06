02b1e003b05f1b4940bd032ccfc691fbde9a5a08
ysknk <31568245+ysknk@users.noreply.github.com>
2022-12-06 19:47:20 +0900

    Add files

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
 # glob-stylus
-glob stylus cli
+
+## Description
+
+stylusをcli化。globで複数ファイルを指定して書き出しできるようにしたもの。  
+引数を渡すことでstylusを実行することができる。  
+
+## Requirement
+
+* Node.js -> check cmd `node -v`
+
+## Install
+
+```sh
+npm i -D https://github.com/ysknk/glob-stylus.git
+```
+
+## Usage
+
+### add script in package.json
+
+```json
+{
+  "scripts": {
+    "stylus": "stylus"
+  },
+}
+```
+
+```sh
+# check arguments help
+npm run stylus -- --help
+```
+
+### ex) set options
+
+project root `.delrc.js`  
+or  
+cli `npm run stylus -- -cwd "./test/" -src "**/[!_]*.js"
+`
---
+++ index.js
@@ -0,0 +1,42 @@
+#!/usr/bin/env node
+
+'use strict'
+
+/* @author ysknk */
+
+import fs from 'fs'
+import path from 'path'
+import glob from 'glob'
+
+import stylus from 'stylus'
+import nib from 'nib'
+import utils from 'node-package-utilities'
+
+import argv from './lib/arguments.js'
+
+glob.sync(argv.src, {
+  ignore: argv.ignore,
+  cwd: argv.cwd
+}).map((key) => {
+  const filename = key.replace(/\.[^/.]+$/, '')
+  const filepath = path.resolve(argv.cwd, `${key}`)
+
+  fs.readFile(filepath, argv.enc, (err, data) => {
+    if (err) { return }
+
+    argv.exec({
+      stylus,
+      nib,
+      fs,
+      path,
+
+      argv,
+
+      filepath,
+      filename,
+
+      data
+    })
+  });
+});
+
